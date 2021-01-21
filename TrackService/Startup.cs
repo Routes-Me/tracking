79b1327d5aca67e191329c80c1dff08c0de81ae6
vivian tharakan <vtharaka@routesme.com>
2021-01-21 15:52:46 +0300

    Removed Cronjobs & Hub update for sending last location of the feeds

--- TrackService/Startup.cs
+++ TrackService/Startup.cs
@@ -67,19 +67,19 @@ namespace TrackService
                 hubOptions.EnableDetailedErrors = true;
             });
 
-            services.AddCronJob<SyncCoordinates>(c =>
-            {
-                c.TimeZoneInfo = TimeZoneInfo.Utc;
-               // c.CronExpression = @"0 1 */1 * * "; // Run every day at 1 AM
-                c.CronExpression = @"*/4 * * * *"; // Run every 4 minutes
-            });
+            //services.AddCronJob<SyncCoordinates>(c =>
+            //{
+            //    c.TimeZoneInfo = TimeZoneInfo.Utc;
+            //   // c.CronExpression = @"0 1 */1 * * "; // Run every day at 1 AM
+            //    c.CronExpression = @"*/4 * * * *"; // Run every 4 minutes
+            //});
 
-            services.AddCronJob<SyncVehicles>(c =>
-            {
-                c.TimeZoneInfo = TimeZoneInfo.Utc;
-                 //c.CronExpression = @"0 3 */7 * * "; // Run every 7 day at 3 AM
-                c.CronExpression = @"*/8 * * * *"; // Run every 8 minutes
-            });
+            //services.AddCronJob<SyncVehicles>(c =>
+            //{
+            //    c.TimeZoneInfo = TimeZoneInfo.Utc;
+            //     //c.CronExpression = @"0 3 */7 * * "; // Run every 7 day at 3 AM
+            //    c.CronExpression = @"*/8 * * * *"; // Run every 8 minutes
+            //});
 
             var appSettingsSection = Configuration.GetSection("AppSettings");
             services.Configure<AppSettings>(appSettingsSection);
